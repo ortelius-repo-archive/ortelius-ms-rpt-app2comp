d63c7438fb60ad5611ab8ea4d4f7f7cd8458f0fe
ayesha khaliq <ayeshakhaliqrana@gmail.com>
2020-10-25 03:07:47 +0500

    Added Cloud.yaml file that will create and push the docker image for the repo to quay.io org (#3)
    * Added cloud.yaml file to push the docker image to quay.io organization.
    
    * updated file path.
    
    * Added Cloud.yaml file that will create and push the docker image for this repo to quay.io org.
    
    Associated issue: https://github.com/ortelius/ortelius/issues/114
    
    Some other minor fixes.
    Updated the image path in the helm chart by pointing it to Quay hosted image.
    Updated the Readiness and liveness probe.
    
    * Waiteress-server has some CVEs that we can't fix.
    Hence removed the waitress code serving part from the code
    Added a new server https://github.com/tiangolo/meinheld-gunicorn-flask-docker
    
    Now the app while running from docker will be serverd by gunicorn
    
    * Updated read me
    
    * Update README.md
    
    Co-authored-by: ayesha <ak.emumba@psiquantum.com>

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
-
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-from waitress import serve
 
 app = Flask(__name__)
 api = Api(app)
@@ -56,7 +54,4 @@ api.add_resource(Capitals, '/capitals/<country_name>')
 
 
 if __name__ == '__main__':
-    if (os.getenv('FLASK_DEV_ENV', None) is not None):
         app.run(host="0.0.0.0", port=5000)
-    else:
-        serve(app, host='0.0.0.0', port=5000)
